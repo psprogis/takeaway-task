3979dc9cdd368e7689a09c3114f0757f60d9b9a7
Pavlo Sprogis <pavlo.sprogis@alianza.com>
2020-08-09 20:34:02 +0300

    add DeliveryPage.processOrder, rename RestaurantsPage -> SearchRestaurantPage

--- specs/orders.spec.js
+++ specs/orders.spec.js
@@ -11,26 +11,41 @@ describe('orders feature', () => {
     });
 
     it('should allow to create an order', async () => {
-        const restaurantsPage = await this.mainPage.findRestaurants({ address: '8888', selectOption: '8888 Alpha' });
+        const searchRestaurantPage = await this.mainPage
+            .findRestaurants({ address: '8888', selectOption: '8888 Alpha' });
 
-        expect(await restaurantsPage.getRestaurantsCount())
+        expect(await searchRestaurantPage.getRestaurantsCount())
             .toBeGreaterThan(0, 'did not find any restaurants on the restaurants page');
 
-        const testRestaurant = await restaurantsPage.selectRestaurant({ name: 'TEST Restaurant Selenium' });
+        const testRestaurant = await searchRestaurantPage.selectRestaurant({ name: 'TEST Restaurant Selenium' });
         await testRestaurant.addItem({ name: 'Salami' });
 
         const deliveryPage = await testRestaurant.placeOrder();
-        await deliveryPage.fillInForm({
+        await deliveryPage.processOrder({
             where: { address: 'main street 2415', postcode: '8888AA', city: 'Enschede' },
             who: { name: 'TestUSer', email: 'testuser@test.test', phone: '1234567890' },
             when: { time: 'asap' },
-            payment: { cash: true }, // FIXME
+            payment: { type: 'cash' },
         });
-
-        // select cash payment and closest amount
 
         // assert order reference number
 
         await browser.sleep(5000);
     });
+
+    xit('test', async () => {
+        await browser.get('https://www.thuisbezorgd.nl/en/checkout-order-qa-restaurant-selenium');
+
+        await browser.sleep(2000);
+
+        $('#iremarks').sendKeys('tesing');
+
+        const payment = await $('.paymentbuttonwrapper.payment-method-type-online.paymentmethod3');
+        await browser.executeScript('arguments[0].scrollIntoView(true)', payment);
+
+        await browser.sleep(2000);
+        await payment.click();
+
+        await browser.sleep(5000);
+    }, 5 * 60 * 1000);
 });
